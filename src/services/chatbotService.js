5f39ef28c0ecef3adc63c7a84e4910a0ca5d54b2
Le <charon@MacBook-Pro-cua-Le.local>
2022-08-15 00:06:36 +0700

    updated

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -75,11 +75,6 @@ let callSendAPIv2 = (sender_psid, title, subtitle, imageUrl, redirectUrl) => {
                                             "url": redirectUrl,
                                             "title": "Xem chi tiết"
                                         },
-                                        {
-                                            "type": "phone_number",
-                                            "title": "Gọi hotline",
-                                            "payload": "+8498549864"
-                                        }
                                     ]
                                 }
                             ]
@@ -193,20 +188,24 @@ let handleEntity = async (name, sender_psid, entity) => {
     switch (name) {
         case "intent":
             if (entity.value === 'doctors') {
-                await callSendAPI(sender_psid, "Bạn đang tìm kiếm thông tin về bác sĩ, xem thêm ở link bên dưới nhé.");
+                let response1 = { "text": `Bạn đang tìm kiếm thông tin về bác sĩ, xem thêm ở link bên dưới nhé.` }
+                await callSendAPI(sender_psid, response1);
                 let title = "P-Covid Care";
                 let subtitle = 'Thông tin bác sĩ làm việc tại P-Covid Care';
                 await callSendAPIv2(sender_psid, title, subtitle, DOCTOR_IMAGE_URL, DOCTOR_URL);
             }
             break;
         case "booking":
-            await callSendAPI(sender_psid, "Bạn đang cần đặt lịch khám bệnh, xem thêm hướng dẫn đặt lịch chi tiết ở link bên dưới nhé.");
+            let response2 = { "text": `Bạn đang cần đặt lịch khám bệnh, xem thêm hướng dẫn đặt lịch chi tiết ở link bên dưới nhé.` }
+            await callSendAPI(sender_psid, response2);
             await callSendAPIv2(sender_psid, "Đặt lịch khám bệnh", "Hướng dẫn đặt lịch khám bệnh tại P-Covid Care", BOOKING_IMAGE_URL, BOOKING_URL);
         case "info":
-            await callSendAPI(sender_psid, "Bạn đang tìm hiểu về thông tin website, xem thêm ở link bên dưới nhé.");
+            let response3 = { "text": `Bạn đang tìm hiểu về thông tin website, xem thêm ở link bên dưới nhé.` }
+            await callSendAPI(sender_psid, response3);
             await callSendAPIv2(sender_psid, "Thông tin website", "Thông tin website P-Covid Care", INFOWEBSITE_IMAGE_URL, INFOWEBSITE_URL);
         default:
-            await callSendAPI(sender_psid, "Rất tiếc bot chưa được hướng dẫn để trả lời câu hỏi của bạn. Để được hỗ trợ, vui lòng truy câp:");
+            let response4 = { "text": `Rất tiếc bot chưa được hướng dẫn để trả lời câu hỏi của bạn. Để được hỗ trợ, vui lòng truy câp:` }
+            await callSendAPI(sender_psid, response4);
             await callSendAPIv2(sender_psid, "Hỗ trợ khách hàng", "Thông tin hỗ trợ khách hàng P-Covid Care", DEFAULT_IMAGE_URL, DEFAULT_URL);
     }
 };
